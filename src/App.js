9173e571b1e90968112fc072d7e786726e7894cf
unknown <ilhemhmida0@gmail.com>
2020-10-06 19:33:43 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import cat3 from "./cat3.jpg";
+import ReactPlayer from 'react-player';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div style={{border:'solid 1px black',maxWidth:'100vw'}}>
+
+<h1 className="title red">Your name here</h1>
+<div></div>
+<img className="image"src={cat3} ></img>
+<div></div>
+<img className="image" src="/images/cat1.jpg"></img>
+<div className='player-wrapper'>
+        <ReactPlayer
+          className='react-player'
+          url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
+          width='300px'
+          height='300px'
+        />
+      </div>
+
+</div>
+
+
       </header>
     </div>
   );
